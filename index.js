8f46053f2b08c24db8eff290491643a53626ee93
suisho <suisho000000@gmail.com>
2014-04-03 01:23:23 +0900

    declaration computing

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
-var visit = require('rework-visit')
+var walk = require("rework-walk")
+var defaults = require("defaults")
+// put regexp local value for performance.
+var importantRegexp = new RegExp("imporant\!")
+importantRegexp.compile()
 
-module.exports = function(style, rework){
-  return function(style){
-    visit(style, function(declarations){
-      console.log(declarations)
-    })
-  }
+module.exports = function(style){
+  // compute same property
+  walk(style, function(rules, node){
+    rules.declarations = computeDeclaration(rules.declarations)
+  })
+}
+
+
+var computeDeclaration = function(decls){
+  var flat = {}
+  var important = {}
+  var order = []
+
+  decls.forEach(function(decl){
+    // add to hash
+    var container = isImportant(decl.value) ? important : flat
+    container[decl.property] = decl
+
+    // detect order
+    var orderIdx = order.indexOf(decl.property)
+    if(orderIdx > -1){
+      order.splice(orderIdx, 1)
+    }
+    order.push(decl.property)
+  })
+
+  var computed = []
+  // ordering
+  order.forEach(function(prop){
+    var decl = flat[prop]
+    if(important[prop]){
+      decl = important[prop]
+    }
+    computed.push(decl)
+  })
+  return computed
+}
+
+
+var isImportant = function(value){
+  return importantRegexp.test(value)
 }
--- test/index.js
+++ test/index.js
@@ -1,7 +1,16 @@
 var rework = require("rework")
+var compute = require("../index")
 var fs = require("fs")
-var fixture = fs.readFileSync("../fixture/sample.css", "utf-8")
+var assert = require("assert")
+
+var assertFixture = function(dir){
+  var input = fs.readFileSync("./fixture/"+dir+"/input.css", "utf-8")
+  var output = fs.readFileSync("./fixture/"+dir+"/output.css", "utf-8")
+  assert(output, rework(input).use(compute).toString())
+}
+
 describe("", function(){
-  it("", function(){
+  it("basic usage", function(){
+    assertFixture("basic")
   })
 })
