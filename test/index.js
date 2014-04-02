446db57386b9913f04bd70037bb6989fa0795a4d
suisho <suisho000000@gmail.com>
2014-04-03 01:31:08 +0900

    fix test

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var assert = require("assert")
 var assertFixture = function(dir){
   var input = fs.readFileSync("./fixture/"+dir+"/input.css", "utf-8")
   var output = fs.readFileSync("./fixture/"+dir+"/output.css", "utf-8")
-  assert(output, rework(input).use(compute).toString())
+  assert.equal(output, rework(input).use(compute).toString())
 }
 
 describe("", function(){
